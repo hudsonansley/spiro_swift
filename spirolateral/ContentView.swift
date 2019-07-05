88726f8ac1edff9f35f00b42af3a3b68199a8449
Hudson Ansley <hudson@speakaboos.com>
2019-07-05 11:02:20 -0400

    Initial Commit

---
+++ spirolateral/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  spirolateral
+//
+//  Created by Hudson Ansley on 7/5/19.
+//  Copyright © 2019 hlamedia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
